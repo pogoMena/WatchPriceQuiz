03066ebda89978e3a0a7fcf75c479b0b9c1f87d4
pogoMena <xpogomena@gmail.com>
2023-02-20 23:43:47 -0500

    added function to get next 100 posts

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -3,12 +3,21 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export const Quiz = () => {
+    const [searchStart, setSearchStart] = useState(100);
   //secret: K07J2yPPhzObBT1wyXTFdGTmjzNOEg
   const [currentWatchParent, setCurrentWatchParent] = useState("");
   const [currentWatch, setCurrentWatch] = useState("");
-  const [currentWatchObj, setCurrentWatchObj] = useState({ image: null, price: null, index: null });
+  const [currentWatchObj, setCurrentWatchObj] = useState({
+    image: null,
+    price: null,
+    index: null,
+  });
 
-  const [nextWatchObject, setNextWatchObject] = useState({ image: null, price: null, index: null });
+  const [nextWatchObject, setNextWatchObject] = useState({
+    image: null,
+    price: null,
+    index: null,
+  });
   const [nextWatchParent, setNextWatchParent] = useState("");
 
   const [watchPrice, setWatchPrice] = useState(0);
@@ -31,31 +40,22 @@ export const Quiz = () => {
 
   const [correctAnswerClass, setCorrectAnswerClass] = useState("answer");
   const [wrongAnswerClass, setWrongAnswerClass] = useState("answer");
-  const [spinClass, setSpinClass] = useState('')
+  const [spinClass, setSpinClass] = useState("");
 
-  const [ currentImage, setCurrentImage] = useState("");
-
-
+  const [currentImage, setCurrentImage] = useState("");
 
   const NextWatch = async (optionalPostNumber = postIndex) => {
-
-
     //resets the image to the first in the index
     setImageIndex(0);
     if (currentWatchObj.image && nextWatchObject.image) {
-
       let tempimage = document.getElementById("activeWatch");
       tempimage.style.transform = `rotate(0deg)`;
       setImageRotation(0);
     }
 
-
-
-
     //If posts exist and it isnt and outofbounds exception
     if (posts && optionalPostNumber < posts.length) {
-
-      console.log('got posts');
+      console.log("got posts");
       //I need to do something here
 
       /*
@@ -67,53 +67,46 @@ export const Quiz = () => {
       setNextWatchParent(posts[optionalPostNumber]);
       setPostIndex(optionalPostNumber + 1);
       let wp = posts[optionalPostNumber].data;
-      var tempWatchObject = { price: null, image: null, index: null }
-
-
+      var tempWatchObject = { price: null, image: null, index: null };
 
       fetch("https://www.reddit.com" + wp.permalink + ".json").then((res) => {
         res.json().then(async (data) => {
           if (data) {
-            console.log('got data');
+            console.log("got data");
 
             let price = await GetPrice(data, optionalPostNumber);
 
-
             console.log(price);
-            console.log('yo')
+            console.log("yo");
 
             if (price) {
-              console.log('it thinks theres a price')
+              console.log("it thinks theres a price");
               tempWatchObject.price = price;
               //setNextWatchObject({price: success})
-              console.log(price)
-
+              console.log(price);
 
               /*
 
               WE ARE GOOD TO THIS POINT
 
               */
-              console.log('image is next')
-              let image = await GetImages(data[0].data.children[0].data)
+              console.log("image is next");
+              let image = await GetImages(data[0].data.children[0].data);
 
-
-              console.log(image)
+              console.log(image);
 
               if (!image) {
-
-                console.log('unsuccessful image gang')
+                console.log("unsuccessful image gang");
                 //Unsuccessful. dont set nextWatchObject and go to the next item
                 NextWatch(optionalPostNumber + 1);
               } else {
                 tempWatchObject.image = image;
 
-
                 let index = await GenerateAnswers(price);
-                console.log(index)
+                console.log(index);
 
                 if (index) {
-                  tempWatchObject.index = index
+                  tempWatchObject.index = index;
                   console.log(tempWatchObject);
                   if (!currentWatchObj) {
                     //If statement if there isnt a currentwatchobj already
@@ -127,38 +120,27 @@ export const Quiz = () => {
                 } else {
                   //no index? idk
                 }
-
               }
-
             } else {
-              console.log('fail')
+              console.log("fail");
               NextWatch(optionalPostNumber + 1);
             }
-
-
-
           }
-        })
-      })
-
+        });
+      });
     } else {
+      console.log("skippin the whole thing are we");
+      setPostIndex(0);
+      //let random = await fetchData();
+      //console.log(random)
 
-      console.log('skippin the whole thing are we')
     }
-
   };
 
-
   async function GetPrice(data, optionalPostNumber) {
-
-
-
-
     /***
      * This is to get the comment that will have the price in it
-     */if (data[1].data.children) {
-
-
+     */ if (data[1].data.children) {
       setCurrentWatch(data[1].data.children);
       let postComments = data[1].data.children;
       let keepItGoing = true;
@@ -195,7 +177,6 @@ export const Quiz = () => {
                   }
                 });
                 if (lowest < nextLowest / 2) {
-
                   //setWatchPrice(nextLowest);
                   //setCurrentWatchObj({ price: nextLowest })
 
@@ -207,7 +188,7 @@ export const Quiz = () => {
                   setWatchPrice(lowest);
                   //setCurrentWatchObj({ price: lowest })
                   GenerateAnswers(lowest);
-                  console.log(lowest)
+                  console.log(lowest);
                   finalResult = lowest;
                   keepItGoing = false;
                 }
@@ -222,27 +203,21 @@ export const Quiz = () => {
               }
             } else {
               setPotentialAnswers([]);
+              console.log(post.data);
               keepItGoing = false;
             }
-
           }
         }
       });
       return finalResult;
-
     } else {
+      console.log();
 
       return false;
     }
-
   }
 
-
-
   const GetImages = async (data) => {
-
-
-
     if (data.media_metadata) {
       let metaData = data.media_metadata;
       let items = data.gallery_data.items;
@@ -263,40 +238,33 @@ export const Quiz = () => {
       //setImages(ImagesArray);
       setImageIndex(0);
       setCurrentImage(ImagesArray[0]);
-      return ({ images: ImagesArray, image: ImagesArray[0] });
+      return { images: ImagesArray, image: ImagesArray[0] };
     } else {
-
       setImages([]);
       let tempUrl = data.url_overridden_by_dest;
 
-
-
       if (tempUrl.includes("v.redd")) {
         //If it is a video link
-        console.log('vreddit')
-        return (false);
+        console.log("vreddit");
+        return false;
       } else if (tempUrl.includes("imgur")) {
         //If it is an imgur link
-        console.log('imgur else if')
+        console.log("imgur else if");
         if (data.media.oembed) {
-          console.log('oembed');
-          let fullPic = data.media.oembed.thumbnail_url.replace("?fb", "")
+          console.log("oembed");
+          let fullPic = data.media.oembed.thumbnail_url.replace("?fb", "");
           setCurrentImage(fullPic);
-          return ({ image: fullPic });
+          return { image: fullPic };
         } else {
-          return (false);
+          return false;
         }
-
       } else {
         setCurrentImage(tempUrl);
         //setCurrentWatchObj({ image: tempUrl });
-        return ({ image: tempUrl });
+        return { image: tempUrl };
       }
     }
-
-  }
-
-
+  };
 
   const updatePosts = (newPosts) => {
     setPosts(newPosts);
@@ -306,14 +274,11 @@ export const Quiz = () => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-  }
-
+  };
 
   const roundToFive = (number) => {
-
-    return (number - (number % 5))
-
-  }
+    return number - (number % 5);
+  };
   const GenerateAnswers = async (answer) => {
     //0 is lowest
     //1 second lowest
@@ -358,20 +323,14 @@ export const Quiz = () => {
         break;
     }
     console.log(answers);
-    return ({ answers: answers, index: index });
+    return { answers: answers, index: index };
     //setPotentialAnswers(answers);
   };
 
-
   const imageChange = (index) => {
-
     setImageIndex(index);
-    setCurrentImage(currentWatchObj.image.images[index])
-
+    setCurrentImage(currentWatchObj.image.images[index]);
   };
-
-
-
 
   const rotateImage = () => {
     const image = document.getElementById("activeWatch");
@@ -382,10 +341,10 @@ export const Quiz = () => {
     image.style.transform = `rotate(${imageRotateTemp}deg)`;
   };
   function timeout(delay) {
-    return new Promise(res => setTimeout(res, delay));
+    return new Promise((res) => setTimeout(res, delay));
   }
   const handleGlow = async () => {
-    const answers = document.getElementsByClassName('answer');
+    const answers = document.getElementsByClassName("answer");
     setCorrectAnswerClass("correctanswer");
     setWrongAnswerClass("wronganswer");
 
@@ -393,21 +352,17 @@ export const Quiz = () => {
     setCorrectAnswerClass("answer");
     setWrongAnswerClass("answer");
     NextWatch();
-    
-    
+
     return true;
-  }
-
-
+  };
 
   const rightAnswer = async () => {
     const image = document.getElementById("activeWatch");
     image.style.transform = `rotate(1080deg)`;
 
-    
-    image.style.animationName = 'spin'
+    image.style.animationName = "spin";
     await handleGlow();
-    
+
     let correctTemp = numCorrect + 1;
     let totalTemp = numTotal + 1;
     let percentageTemp = Math.round((correctTemp / totalTemp) * 100);
@@ -415,7 +370,6 @@ export const Quiz = () => {
     setNumCorret(correctTemp);
     setNumTotal(totalTemp);
     setPercentage(percentageTemp);
-    
   };
 
   const wrongAnswer = () => {
@@ -429,13 +383,12 @@ export const Quiz = () => {
 
     setNumTotal(totalTemp);
     setPercentage(percentageTemp);
-
-    
   };
 
-  const fetchData = () => {
-    fetch("https://www.reddit.com/r/watchexchange/new/.json?limit=100").then(
+  const fetchData = async () => {
+    fetch(`https://www.reddit.com/r/watchexchange/new/.json?limit=100?count-${searchStart}`).then(
       (res) => {
+        setSearchStart(searchStart+100)
         if (res.status !== 200) {
           console.log("error");
           return;
@@ -444,15 +397,17 @@ export const Quiz = () => {
           if (data) {
             updatePosts(data.data.children);
             NextWatch();
+            return true;
           }
+          return false;
         });
       }
+      
     );
   };
 
   const PictureZoom = (event) => {
-    
-    let leftButton = document.getElementsByClassName('leftButton')[0];
+    let leftButton = document.getElementsByClassName("leftButton")[0];
     console.log(leftButton);
     let original = document.querySelector("#activeWatch"),
       magnified = document.querySelector("#picZoom"),
@@ -463,7 +418,7 @@ export const Quiz = () => {
       imgHeight = original.offsetHeight,
       xperc = (x / imgWidth) * 100,
       yperc = (y / imgHeight) * 100;
-      console.log(original.offsetLeft);
+    console.log(original.offsetLeft);
 
     //lets user scroll past right edge of image
     if (x > 0.01 * imgWidth) {
@@ -480,8 +435,7 @@ export const Quiz = () => {
 
     style.left = x - 180 + "px";
     style.top = y - 180 + "px";
-    
-  }
+  };
 
   useEffect(() => {
     if (posts) {
@@ -491,112 +445,100 @@ export const Quiz = () => {
     }
   }, [posts]);
 
-
-  if(currentWatchObj){
-
-  
-  return (
-    <>
-      <div className="pageWrapper">
-        <div className="questionDisplay">
-          <div className="leftButton">
-            {currentWatchObj.image && imageIndex > 0 && (
-              <button
-                className=""
-                onClick={() => {
-                  imageChange(imageIndex - 1);
-                }}>
-                {String.fromCharCode(8592)}
-              </button>
+  if (currentWatchObj) {
+    return (
+      <>
+        <div className="pageWrapper">
+          <div className="questionDisplay">
+            {currentWatchObj.image && (
+              <div className="activeWatch">
+                {currentWatchObj.image && imageIndex > 0 && (
+                  <button
+                    className="leftButton"
+                    onClick={() => {
+                      imageChange(imageIndex - 1);
+                    }}>
+                    {String.fromCharCode(8592)}
+                  </button>
+                )}
+                <img
+                  src={currentImage}
+                  alt="watch"
+                  id="activeWatch"
+                  className={spinClass}
+                  onMouseMove={(event) => {
+                    PictureZoom(event);
+                  }}
+                />
+                {currentWatchObj.image &&
+                  currentWatchObj.image.images &&
+                  imageIndex < currentWatchObj.image.images.length - 1 && (
+                    <button
+                      className="rightButton"
+                      onClick={() => {
+                        imageChange(imageIndex + 1);
+                      }}>
+                      {String.fromCharCode(8594)}
+                    </button>
+                  )}
+                <div
+                  className="picZoom"
+                  id="picZoom"
+                  alt="?"
+                  style={{
+                    background: `url(${currentImage}) no-repeat #FFF`,
+                  }}></div>
+              </div>
             )}
           </div>
-
-          {currentWatchObj.image && (
-            <div className="activeWatch">
-              <img
-                src={currentImage}
-                alt="watch"
-                id="activeWatch"
-                className={spinClass}
-                onMouseMove={(event) => {
-                  PictureZoom(event);
-                }}
-              />
-              <div
-                className="picZoom"
-                id="picZoom"
-                alt="?"
-                style={{
-                  background: `url(${currentImage}) no-repeat #FFF`,
-                }}></div>
-            </div>
-          )}
-
-          <div className="rightButton ">
-            {currentWatchObj.image &&
-              currentWatchObj.image.images &&
-              imageIndex < currentWatchObj.image.images.length - 1 && (
+          <div className="answersParent">
+            <div className="toolBox">
+              <div className="">
                 <button
-                  className=""
+                  className="nextWatch"
                   onClick={() => {
-                    imageChange(imageIndex + 1);
+                    NextWatch(postIndex);
                   }}>
-                  {String.fromCharCode(8594)}
+                  Next watch
                 </button>
-              )}
+              </div>
+              <div className="">
+                <button className="rotate" onClick={rotateImage}>
+                  Rotate
+                </button>
+              </div>
+              <div className="score">Total: {percentage}%</div>
+            </div>
+            {currentWatchObj.index && (
+              <div className="answersDiv">
+                {currentWatchObj.index.answers.map((answer, index) => {
+                  if (index === currentWatchObj.index.index) {
+                    return (
+                      <button
+                        className={correctAnswerClass}
+                        onClick={rightAnswer}
+                        key={index}>
+                        ${answer}
+                      </button>
+                    );
+                  } else {
+                    return (
+                      <button
+                        className={wrongAnswerClass}
+                        onClick={wrongAnswer}
+                        key={index}>
+                        ${answer}
+                      </button>
+                    );
+                  }
+                })}
+              </div>
+            )}
           </div>
         </div>
-        <div className="answersParent">
-          <div className="toolBox">
-            <div className="">
-              <button
-                className="nextWatch"
-                onClick={() => {
-                  NextWatch(postIndex);
-                }}>
-                Next watch
-              </button>
-            </div>
-            <div className="">
-              <button className="rotate" onClick={rotateImage}>
-                Rotate
-              </button>
-            </div>
-            <div className="score">Total: {percentage}%</div>
-          </div>
-          {currentWatchObj.index && (
-            <div className="answersDiv">
-              {currentWatchObj.index.answers.map((answer, index) => {
-                if (index === currentWatchObj.index.index) {
-                  return (
-                    <button
-                      className={correctAnswerClass}
-                      onClick={rightAnswer}
-                      key={index}>
-                      ${answer}
-                    </button>
-                  );
-                } else {
-                  return (
-                    <button
-                      className={wrongAnswerClass}
-                      onClick={wrongAnswer}
-                      key={index}>
-                      ${answer}
-                    </button>
-                  );
-                }
-              })}
-            </div>
-          )}
-        </div>
-      </div>
-    </>
-  );}else{
-
-    return(
-        <h1>Loading...</h1>
-    )
-}
-}
-
+      </>
+    );
+  } else {
+    return <h1>Loading...</h1>;
+  }
+};
